cc6aec2c9e4d496ca4fe2436b2db4968351bf2c7
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-31 02:32:37 +0300

    Define database schema: tables for animals, owners, and species

--- schema.sql
+++ schema.sql
@@ -6,3 +6,23 @@ CREATE TABLE animals (id INT PRIMARY KEY,name varchar(255),date_of_birth date,es
 ALTER TABLE animals
 ADD
 COLUMN species VARCHAR(255);
+
+-- Create the 'owners' table
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+-- Create the 'species' table
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Modify the 'animals' table
+
+    ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INTEGER REFERENCES species(id),
+    ADD COLUMN owner_id INTEGER REFERENCES owners(id);
