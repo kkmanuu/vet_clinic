5bfb4239ee4c4bfd307567de2b405c8e6c35172e
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-31 02:31:51 +0300

    Add SQL queries to retrieve data

--- queries.sql
+++ queries.sql
@@ -158,3 +158,37 @@ SELECT species, AVG(escape_attempts) AS avg_escape_attempts
 FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+SELECT name FROM animals A
+INNER JOIN owners O
+ON A.owner_id = O.id
+WHERE O.full_name = 'Melody Pond';
+
+SELECT A.name FROM animals A
+INNER JOIN species S
+ON A.species_id = S.id
+WHERE S.name = 'Pokemon';
+
+SELECT full_name, name FROM owners O
+LEFT JOIN animals A
+ON A.owner_id = O.id;
+
+
+SELECT COUNT(*), S.name FROM animals A
+INNER JOIN species S
+ON A.species_id = S.id
+GROUP BY S.name;
+
+SELECT A.name FROM animals A
+INNER JOIN species S ON A.species_id = S.id
+INNER JOIN owners O ON A.owner_id = O.id
+WHERE O.full_name = 'Jennifer Orwell' AND S.name = 'Digimon';
+
+SELECT name FROM animals A
+INNER JOIN owners O ON A.owner_id = O.id
+WHERE O.full_name = 'Dean Winchester' AND escape_attempts = 0;
+
+SELECT COUNT(*) as count, full_name FROM animals A
+INNER JOIN owners O ON A.owner_id = O.id
+GROUP BY O.full_name
+ORDER BY count DESC;
