4537e42caa3d5f8f2889681c09e586530c21d2f4
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-08-31 17:52:54 +0300

    Use primary key, foreign key mechanism for joining tables and use realtionship in tables

--- schema.sql
+++ schema.sql
@@ -26,3 +26,9 @@ CREATE TABLE species (
     DROP COLUMN species,
     ADD COLUMN species_id INTEGER REFERENCES species(id),
     ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+    -- JOIN TABLES
+CREATE TABLE vets(id SERIAL PRIMARY KEY,name VARCHAR(255),age INTEGER,date_of_graduation date);
+CREATE TABLE specializations( vets_id INT REFERENCES vets(id),species_id INT REFERENCES species(id));
+
+CREATE TABLE visits(vet_id INT REFERENCES vets(id),animal_id INT REFERENCES animals(id),date_of_visit date);
