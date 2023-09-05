8cb16a599f2f490a8f2c7c61c95d80442207ea2a
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-05 09:00:01 +0300

    Update database schema and added indexes.

--- schema.sql
+++ schema.sql
@@ -32,3 +32,10 @@ CREATE TABLE vets(id SERIAL PRIMARY KEY,name VARCHAR(255),age INTEGER,date_of_gr
 CREATE TABLE specializations( vets_id INT REFERENCES vets(id),species_id INT REFERENCES species(id));
 
 CREATE TABLE visits(vet_id INT REFERENCES vets(id),animal_id INT REFERENCES animals(id),date_of_visit date);
+
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX visits_animal_id ON visits(animal_id);
+CREATE INDEX visits_vet_id ON visits(vet_id);
+CREATE INDEX owner_email_index ON owners(email);
