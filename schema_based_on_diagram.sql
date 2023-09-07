97e338e54690ec7fc3f8a378ab9eb80ea6922648
Briton Otieno <ojwangbriton@gmail.com>
2023-09-07 13:28:42 +0300

    Add indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -47,3 +47,10 @@ CREATE TABLE medical_join_treatments (
     CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
     CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE
 );
+
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoice_items (treatment_id);
+CREATE INDEX ON medical_join_treatments (medical_history_id);
+CREATE INDEX ON medical_join_treatments (treatment_id);
