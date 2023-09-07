25ac86a0fe31376f1020b6d5e732f5fbe6e817de
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-07 12:40:03 +0300

    Add database schema for a veterinary clinic management system and create indexes for all foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,49 +1,49 @@
 CREATE DATABASE vet_clinic;
 \c vet_clinic;
 
-CREATE TABLE patients (
-    id serial PRIMARY KEY,
+CREATE TABLE patients(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(150),
-    date_of_birth DATE
+    date_of_birth date
 );
 
-CREATE TABLE medical_histories (
-    id serial PRIMARY KEY,
-    admitted_at TIMESTAMP,
+CREATE TABLE medical_histories(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
     patient_id integer,
     status VARCHAR(50),
-    CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE CASCADE
+    CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE invoices (
-    id serial PRIMARY KEY,
-    total_amount DECIMAL(10, 2),
-    generated_at TIMESTAMP,
-    paid_at TIMESTAMP,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount decimal(10, 2),
+    generated_at timestamp,
+    payed_at timestamp,
     medical_history_id integer,
-    CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE
+    CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
 );
 
 CREATE TABLE treatments (
-    id serial PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     type VARCHAR(255),
     name VARCHAR(255)
 );
 
 CREATE TABLE invoice_items (
-    id serial PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     unit_price DECIMAL(10, 2),
     quantity INT,
     total_price DECIMAL(10, 2),
-    invoice_id integer,
-    treatment_id integer,
-    CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices (id) ON DELETE CASCADE,
-    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE
+    invoice_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
+    CONSTRAINT fk_treatments FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE
 );
 
-CREATE TABLE medical_history_treatments (
+CREATE TABLE medical_join_treatments (
     medical_history_id integer,
     treatment_id integer,
-    CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id) ON DELETE CASCADE,
-    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments (id) ON DELETE CASCADE
+    CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
+    CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE
 );
